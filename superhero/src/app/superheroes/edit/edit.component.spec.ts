a56045f3203ed86c9df1985aef7190450733ea33
AnteroGuarinos <a.guarinos.caballero@accenture.com>
2024-06-03 18:08:50 +0200

    fix: add test

--- superhero/src/app/superheroes/edit/edit.component.spec.ts
+++ superhero/src/app/superheroes/edit/edit.component.spec.ts
@@ -1,15 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 import { EditComponent } from './edit.component';
+import { SuperheroesService } from '../../services/superheroes.service';
 
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
+  let mockRouter: any;
+  let mockActivatedRoute: any;
+  let mockSuperheroesService: jasmine.SpyObj<SuperheroesService>;
+
+  beforeEach(async () => {
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+    mockActivatedRoute = { snapshot: { paramMap: { get: () => '1' } } };
+    mockSuperheroesService = jasmine.createSpyObj('SuperheroesService', [
+      'getSuperHeroeById',
+      'updateSuperHero',
+    ]);
+
+    await TestBed.configureTestingModule({
+      declarations: [EditComponent],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: SuperheroesService, useValue: mockSuperheroesService },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditComponent]
-    });
     fixture = TestBed.createComponent(EditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +39,19 @@ describe('EditComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should populate form with hero data on initialization', () => {
+    const mockHeroData = {
+      id: '2',
+      name: 'Superman',
+      image: { url: 'superman.jpg' },
+    };
+    mockSuperheroesService.getSuperHeroeById.and.returnValue(of(mockHeroData));
+
+    component.ngOnInit();
+
+    expect(component.heroData).toEqual(mockHeroData);
+    expect(component.editHeroForm.value).toEqual(mockHeroData);
   });
 });
